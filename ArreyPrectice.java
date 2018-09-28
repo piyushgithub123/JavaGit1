2427075e34f2989615ce74a1b93288116ea68e13
piyushgithub123 <43575683+piyushgithub123@users.noreply.github.com>
2018-09-28 15:37:34 +0530

    Create ArreyPrectice.java

---
+++ ArreyPrectice.java
@@ -0,0 +1,31 @@
+public class ArreyPrectice {
+
+
+	public static void main(String[] args) {
+		
+		
+		//First Way
+		//int ArreyOne[];
+		//ArreyOne = new int[5];
+
+		//Second Way
+		//int ArreyOone[] = new int[5];
+		
+		//Third Way
+		int ArreyOne[] = {10,20,30,40,50};
+		
+		
+		ArreyOne[0] = 10;
+		ArreyOne[1] = 20;
+		ArreyOne[2] = 30;
+		ArreyOne[3] = 40;
+		ArreyOne[4] = 50;
+		
+		
+		for(int i=0;i < ArreyOne.length;i++){
+			System.out.println("Arrey Valye is : "+ArreyOne[i]);
+		}
+		
+	}
+
+}
